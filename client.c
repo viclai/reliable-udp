613e54b132cddd3aed20ecc26717331d64c4a3bf
mahireusufzai <l.a.lakersfan4life@gmail.com>
2016-02-16 04:03:41 -0800

    First attempt at sending

--- client.c
+++ client.c
@@ -20,55 +20,42 @@ int main(int argc, char* argv[])
 {
   // TODO: Implement ./client localhost postNumber fileName
     
-    /*
-     A simple client in the internet domain using TCP
-     Usage: ./client hostname port (./client 192.168.0.151 10000)
-     */
+    // error handling
+    if (argc != 4) {
+        fprintf(stderr, "must provide 4 arguments");
+        exit(0);
+    }
     
-        int sockfd; //Socket descriptor
-        int portno, n;
-        struct sockaddr_in serv_addr;
-        struct hostent *server; //contains tons of information, including the server's IP address
+    //obtain args
+    char *hostname = argv[1];
+    int portno = atoi(argv[2]);
+    char *filename =  argv[3];
+    
+    //create socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0); //create a new socket
+    if (sockfd < 0)
+        error("ERROR opening socket");
+    
+     struct hostent *server = gethostbyname(hostname);
+    if (server == NULL) {
+        fprintf(stderr,"ERROR, no such host\n");
+        exit(0);
+    }
+    
+    struct sockaddr_in serv_addr;
+    memset((char *) &serv_addr, 0, sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET; //initialize server's address
+    bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
+    serv_addr.sin_port = htons(portno);
+
+    
+    if (sendto(sockfd, filename, sizeof(filename), 0, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
+        error("ERROR sending request");
+    
+    printf("Sent request for file w/o local error %s\n", filename);
         
-        char buffer[256];
-        if (argc < 3) {
-            fprintf(stderr,"usage %s hostname port\n", argv[0]);
-            exit(0);
-        }
-        
-        portno = atoi(argv[2]);
-        sockfd = socket(AF_INET, SOCK_STREAM, 0); //create a new socket
-        if (sockfd < 0)
-            error("ERROR opening socket");
-        
-        server = gethostbyname(argv[1]); //takes a string like "www.yahoo.com", and returns a struct hostent which contains information, as IP address, address type, the length of the addresses...
-        if (server == NULL) {
-            fprintf(stderr,"ERROR, no such host\n");
-            exit(0);
-        }
-        
-        memset((char *) &serv_addr, 0, sizeof(serv_addr));
-        serv_addr.sin_family = AF_INET; //initialize server's address
-        bcopy((char *)server->h_addr, (char *)&serv_addr.sin_addr.s_addr, server->h_length);
-        serv_addr.sin_port = htons(portno);
-        
-        if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0) //establish a connection to the server
-            error("ERROR connecting");
-        
-        printf("Please enter the message: ");
-        memset(buffer,0, 256);
-        fgets(buffer,255,stdin);	//read message
-        
-        n = write(sockfd,buffer,strlen(buffer)); //write to the socket
-        if (n < 0)
-            error("ERROR writing to socket");
-        
-        memset(buffer,0,256);
-        n = read(sockfd,buffer,255); //read from the socket
-        if (n < 0) 
-            error("ERROR reading from socket");
-        printf("%s\n",buffer);	//print server's response
-        
+    
+    
         close(sockfd); //close socket
         
         return 0;
