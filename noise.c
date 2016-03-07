41bbe950886d64c9b9b7a91db6cd6b7026289e42
viclai <victor1.lai@gmail.com>
2016-03-06 21:40:22 -0800

    Different Algorithm for Seed Generator

--- noise.c
+++ noise.c
@@ -1,7 +1,13 @@
 #include "noise.h"
 
+#include <stdio.h> // fprintf
+
 #include <cstdlib> // srand, rand
 #include <ctime>   // time
+#include <sstream>
+#include <sys/time.h>
+#include <time.h>
+#include <string>
 
 bool simulatePacketLossCorruption(double prob)
 {
@@ -9,10 +15,23 @@ bool simulatePacketLossCorruption(double prob)
 
   if (prob < 0 || prob > 100)
     return false;
-  srand(time(NULL));
+
+  struct timeval tv;
+  time_t long_time;
+  struct tm *newtime;
+
+  gettimeofday(&tv,0);
+  time(&long_time);
+  newtime = localtime(&long_time);
+  std::ostringstream os;
+  os << newtime->tm_hour << newtime->tm_min << newtime->tm_sec
+     << tv.tv_usec;
+  srand(std::stoul(os.str(), nullptr));
+  //srand(time(NULL));
 
   x = (int)prob;
   r = rand() % 100;
+  //fprintf(stdout, "* Prob = %d, Rand = %d\n", x, r);
 
   if (r <= x - 1)
     return true;
